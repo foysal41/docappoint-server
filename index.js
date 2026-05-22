6d8a64177262be16fcde015edef3aabcd78cf012
foysal41 <mdfoysal.mf33@example.com>
2026-05-22 20:13:17 +0600

    delete api created from here

--- index.js
+++ index.js
@@ -150,7 +150,12 @@ app.get("/booking/:userId" , async (req, res) => {
   res.json(result)
 })
 
+app.delete("/booking/:bookingId" , async (req, res)=> {
+  const {bookingId} = req.params;
+  const result = await bookingCollection.deleteOne({_id:new ObjectId(bookingId)})
+  res.json(result)
 
+})
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
